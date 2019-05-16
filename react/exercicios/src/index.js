2152c713e77e4ad887039df7ee36b10e43dfbcb4
fabiofos <fabiofos@gmail.com>
2019-05-16 14:43:24 +0100

    react calculator

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,4 +1,25 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+// import BomDia from './componentes/BomDia'
 
-ReactDom.render(<h1>React</h1>, document.getElementById('root'))
+// import Multi, {BoaNoite} from './componentes/Multiplos'
+
+// import Saudacao from './componentes/Saudacao'
+
+import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
+
+ ReactDom.render(
+//  <div>
+//  <BomDia nome="paula"/>
+//  <Multi.BoaTarde nome="guilherme"/>
+//  <BoaNoite nome="fabio"/>
+//  </div>
+// <Saudacao tipo="Bom Dia" nome=" Fabio" /> 
+<Pai nome="Fabio" sobrenome="Silva">
+ <Filho nome="Paulo" />
+ <Filho nome="Ana"  />
+ <Filho nome="Claudia" />
+</Pai>
+
+ , document.getElementById('root'))
